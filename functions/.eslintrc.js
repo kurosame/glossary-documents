efa7af4c6e8419c39d2cd4a90a926eebc9906089
kurosame <kurosame1227@gmail.com>
2024-01-03 23:24:12 +0900

    add: Firebase Functions(helloWorld example)

---
+++ functions/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  extends: [
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  settings: {
+    "import/resolver": { typescript: {} },
+  },
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", ["internal", "parent", "sibling", "index"], "type"],
+        pathGroups: [
+          {
+            pattern: "xxx/**",
+            group: "internal",
+            position: "before",
+          },
+        ],
+        distinctGroup: false,
+        pathGroupsExcludedImportTypes: ["builtin"],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+    "import/prefer-default-export": "off",
+  },
+  ignorePatterns: [".eslintrc.js"],
+};
